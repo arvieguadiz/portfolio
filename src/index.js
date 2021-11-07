e9ffa2483793f34367073aeeaf82ac1fecbe856f
Christian Arvie Benito <arvieguadiz@gmail.com>
2021-11-07 12:04:08 +0800

    November 7, 2021 | - Started the home page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Paper, Typography } from '@mui/material';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = (props) => {
+  return (
+    <Paper variant="outlined">
+      <Typography variant="h1">Hello World</Typography>
+    </Paper>
+  );
+};
+
+if (document.getElementById('root')) {
+  const props = Object.assign({}, document.getElementById('root').dataset);
+  ReactDOM.render(<React.StrictMode><App {...props} /></React.StrictMode>, document.getElementById('root'));
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
