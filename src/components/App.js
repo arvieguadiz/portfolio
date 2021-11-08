8ecac3447ffbca06882bb87268e04e4ecedb8ce1
Christian Arvie Benito <arvieguadiz@gmail.com>
2021-11-08 17:01:34 +0800

    November 8, 2021 | - Added Drawer and implented the react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,52 @@
 import React, { createContext, useContext, useMemo, useState } from 'react';
-import { Box, IconButton, Paper, Typography } from '@mui/material';
-import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
-import { Brightness4, Brightness7 } from '@mui/icons-material';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Paper, Toolbar, Typography } from '@material-ui/core';
+import { createTheme, makeStyles, ThemeProvider, useTheme } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
+import {
+  Brightness4,
+  Brightness7,
+  Code as CodeIcon,
+  Email as EmailIcon,
+  Person as PersonIcon,
+  ViewList as ViewListIcon
+} from '@material-ui/icons';
 import logo from '../logo.svg';
-import '../App.css';
+import { map } from 'lodash';
+// import '../App.css';
 // import mainTheme from './config/theme';
 
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: {
+    // ...theme.mixins.toolbar,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(2),
+    height: 240,
+    backgroundColor: deepOrange['A200'],
+    color: '#fff',
+  },
+}));
 
 const MyApp = () => {
   const theme = useTheme();
@@ -47,9 +66,9 @@ const MyApp = () => {
       // }}
       variant="outlined"
     >
-      {theme.palette.mode} mode
+      {theme.palette.type} mode
       <IconButton onClick={colorMode.toggleColorMode}>
-        {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        {theme.palette.type === 'dark' ? <Brightness7 /> : <Brightness4 />}
       </IconButton>
       
       <div className="App">
@@ -73,31 +92,109 @@ const MyApp = () => {
 };
 
 const App = () => {
-  const [ mode, setMode ] = useState('light');
+  const classes = useStyles();
+
+  const [ mode, setMode ] = useState({ type: 'light' });
+
+  const drawerItems = [
+    {
+      icon: <PersonIcon />,
+      label: 'About',
+      to: '/portfolio',
+    },
+    {
+      icon: <ViewListIcon />,
+      label: 'Skills',
+      to: '/skills',
+    },
+    {
+      icon: <CodeIcon />,
+      label: 'Works',
+      to: '/works',
+    },
+    {
+      icon: <EmailIcon />,
+      label: 'Contact',
+      to: '/contact',
+    },
+  ];
 
   const colorMode = useMemo(() => ({
     toggleColorMode: () => {
-      setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      setMode((prevMode) => (prevMode.type === 'light' ? { type: 'dark' } : { type: 'light' }));
     },
   }), [],);
 
   const theme = useMemo(() => createTheme({
-    palette: {
-      mode,
-    },
+    palette: { ...mode, },
   }), [mode],);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        {/* <Paper variant="outlined">
-          <Box m={3}>
-            <Typography variant="h1">Hello World</Typography>
-          </Box>
-        </Paper> */}
-        <MyApp />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    // <ColorModeContext.Provider value={colorMode}>
+    //   <ThemeProvider theme={theme}>
+    //     {/* <Paper variant="outlined">
+    //       <Box m={3}>
+    //         <Typography variant="h1">Hello World</Typography>
+    //       </Box>
+    //     </Paper> */}
+    //     <MyApp />
+    //   </ThemeProvider>
+    // </ColorModeContext.Provider>
+
+    <React.Fragment>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Box className={classes.root}>
+              <CssBaseline />
+              {/* <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                  <Typography variant="h6" noWrap>Title</Typography>
+                </Toolbar>
+              </AppBar> */}
+
+              <Drawer
+                variant="permanent"
+                anchor="left"
+                className={classes.drawer}
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+              >
+                <Box className={classes.toolbar}>
+                  <Typography variant="h5">Christian Arvie</Typography>
+                  <Typography variant="h4" style={{ fontWeight: 'bold' }}>Benito</Typography>
+                  <Typography variant="caption" style={{ color: '#000', paddingTop: 15 }}>Web Developer</Typography>
+                </Box>
+                {/* <Divider /> */}
+                <List>
+                  {
+                    map(drawerItems, (item, index) => {
+                      return (
+                        <ListItem button key={`item-${item.label}`} component={Link} to={item.to}>
+                          <ListItemIcon>{ item.icon }</ListItemIcon>
+                          <ListItemText primary={item.label} />
+                        </ListItem>
+                      );
+                    })
+                  }
+                </List>
+                {/* <Divider /> */}
+              </Drawer>
+
+              <main>
+                <Routes>
+                  <Route path="/portfolio" element={<MyApp />} />
+                  <Route path="/skills" element={<MyApp />} />
+                  <Route path="/works" element={<MyApp />} />
+                  <Route path="/contact" element={<MyApp />} />
+                </Routes>
+              </main>
+            </Box>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </React.Fragment>
   );
 };
 
